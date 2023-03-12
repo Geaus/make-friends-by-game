df01c08140adc246ad5a629892c9aae6b63d90e9
illyaks <95566349+illyaks@users.noreply.github.com>
2023-03-12 22:27:40 +0800

    完善聊天界面

--- src/component/header.js
+++ src/component/header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Menu} from 'antd';
+import type {MenuProps} from "antd";
+import {SearchOutlined, QuestionOutlined} from "@ant-design/icons";
 
+const items : MenuProps['items'] = [
+    {
+        label : '搜索陌生人',
+        key : 'search',
+        icon : <SearchOutlined />,
+    },
+    {
+        label : '活动界面',
+        key : 'active',
+    },
+    {
+        label : '在线帮助',
+        key : 'help',
+        icon : <QuestionOutlined />,
+    }
+]
 export class HeaderStyle extends React.Component {
     render() {
         return (
             <div className={"header"}>
-
+                <div className={"header-name"}>游戏交友平台</div>
+                <div className={"header-menu"}>
+                    <Menu className="menu" items={items} theme={"dark"} mode="horizontal"></Menu>
+                </div>
+                <div className={"header-person"}>个人中心</div>
             </div>
         )
     }
--- src/component/menuBar-left.js
+++ src/component/menuBar-left.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Menu,Layout} from 'antd'
+import type {MenuProps} from "antd";
 
-import {UserAddOutlined,UserDeleteOutlined, UsergroupAddOutlined } from '@ant-design/icons';
+import {
+    UserAddOutlined,
+    UserDeleteOutlined,
+    UsergroupAddOutlined
+} from '@ant-design/icons';
 const { Sider } = Layout;
+
+const items : MenuProps['items'] = [
+    {
+        icon : <UserAddOutlined/>,
+        key : '1'
+    },
+    {
+        icon : <UserDeleteOutlined/>,
+        key : '2'
+    },
+    {
+        icon : <UsergroupAddOutlined/>,
+        key : '3'
+    }
+]
 export class MenuBar_Left extends React.Component {
     render() {
         return (
             <div className={"menuBar-left"}>
-
-                <UserAddOutlined className={"iconStyle"}/>
-
-                <UserDeleteOutlined className={"iconStyle"}/>
-
-                <UsergroupAddOutlined className={"iconStyle"}/>
-
-
+                <Menu items={items} className={"menuLeft"} mode={'vertical'}></Menu>
             </div>
         )
     }
