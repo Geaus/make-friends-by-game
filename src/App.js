5ba63fd4f2bc1c1b0a2665dbd155015dba4d2af1
gaoxj <2061389208@qq.com>
2023-03-12 22:45:09 +0800

    完成搜索界面

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import HomeView from "./view/HomeView";
 import LoginView from"./view/LoginView";
+import SearchView from"./view/SearchView";
 import { BrowserRouter,Routes, Route } from "react-router-dom"
 
 
 function App() {
     return (
-
-        <BrowserRouter>
-            <Routes>
-
-                <Route path="/" element={<LoginView />} />
-                <Route path="/home" element={<HomeView />} />
-
-
-            </Routes>
-        </BrowserRouter>
+       <SearchView/>
+        // <BrowserRouter>
+        //     <Routes>
+        //
+        //         <Route path="/" element={<LoginView />} />
+        //         <Route path="/home" element={<HomeView />} />
+        //
+        //
+        //     </Routes>
+        // </BrowserRouter>
 
     );
 }
