89e7e462bf92e17988bd166de5162a7396208271
gxj2003 <2061389208@qq.com>
2023-05-13 18:41:30 +0800

    1

--- TechPrototype/Spring/src/main/java/com/example/makefriendsbackend/MakeFriendsBackendApplication.java
+++ TechPrototype/Spring/src/main/java/com/example/makefriendsbackend/MakeFriendsBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.makefriendbackend;
+package com.example.makefriendsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MakeFriendBackendApplication {
+public class MakeFriendsBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MakeFriendBackendApplication.class, args);
+        SpringApplication.run(MakeFriendsBackendApplication.class, args);
     }
 
 }
